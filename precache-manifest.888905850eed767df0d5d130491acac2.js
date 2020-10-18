76aea76e9e569f79c4c8b2b7df7cd860183a0192
shruti <shrutipandey27122001@gmail.com>
2020-10-18 15:49:15 +0530

    Updates

---
+++ precache-manifest.888905850eed767df0d5d130491acac2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7392eb779088f0945ad2feb267b759a9",
+    "url": "/keep_clone/index.html"
+  },
+  {
+    "revision": "4bf1b60ef86796cba948",
+    "url": "/keep_clone/static/css/main.2ee95024.chunk.css"
+  },
+  {
+    "revision": "1ee461d98adeb43d0abc",
+    "url": "/keep_clone/static/js/2.bf67eeb7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/keep_clone/static/js/2.bf67eeb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bf1b60ef86796cba948",
+    "url": "/keep_clone/static/js/main.980b50b5.chunk.js"
+  },
+  {
+    "revision": "b31d3fdb67fe9dba9571",
+    "url": "/keep_clone/static/js/runtime-main.8ba35b69.js"
+  }
+]);
